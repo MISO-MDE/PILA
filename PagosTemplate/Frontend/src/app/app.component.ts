2b41b7746a009d5999bb320ab8ec2dc993378a53
rtaimal <rtaimal@gmail.com>
2016-11-05 16:25:18 -0500

    add global styles

--- PagosTemplate/Frontend/src/app/app.component.ts
+++ PagosTemplate/Frontend/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-
-import { FireLoginComponent } from './fire-login/fire-login.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
