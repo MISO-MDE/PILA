42f9cd2636eff0f84aaef8276cfa448d2a01525d
rtaimal <rtaimal@gmail.com>
2016-11-06 15:47:11 -0500

    se agrega base del menu  para el admin del aportante

--- PagosTemplate/Frontend/src/app/homepage/homepage.component.ts
+++ PagosTemplate/Frontend/src/app/homepage/homepage.component.ts
@@ -4,16 +4,30 @@ import {PAsideService} from "../p-aside/p-aside.service";
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.less']
 })
 export class HomepageComponent implements OnInit {
 
+  public menuOptions : Array<any> = [
+    {
+      name:"Pensionados",
+      icon:"glyphicon-user",
+    },
+    {
+      name:"Novedades",
+      icon:"glyphicon-fire"
+    },
+    {
+      name:"Pagos",
+      icon:"glyphicon-usd"
+    }
+  ];
   constructor(private asideService: PAsideService) {
 
   }
 
   ngOnInit() {
-    this.asideService.showAside(HomepageComponent);
+    //this.asideService.showAside(HomepageComponent);
   }
 
 }
