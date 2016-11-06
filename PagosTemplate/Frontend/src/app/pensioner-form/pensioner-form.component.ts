62243571983cd7c908924676e3c00024d3a72bbc
rtaimal <rtaimal@gmail.com>
2016-11-06 17:37:01 -0500

    add forms and  details components

---
+++ PagosTemplate/Frontend/src/app/pensioner-form/pensioner-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensioner-form',
+  templateUrl: './pensioner-form.component.html',
+  styleUrls: ['./pensioner-form.component.css']
+})
+export class PensionerFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
