9e9d9447aa2c449b6b9537f6489ad8409366c6ef
rtaimal <rtaimal@gmail.com>
2016-11-06 19:24:27 -0500

    add forms and  details components

--- PagosTemplate/Frontend/src/app/pensioner-form/pensioner-form.component.ts
+++ PagosTemplate/Frontend/src/app/pensioner-form/pensioner-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pensioner-form',
@@ -6,8 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pensioner-form.component.less']
 })
 export class PensionerFormComponent implements OnInit {
+  public pensionTypeOptions: Array<any> = [
+    {
+      'id':"12",
+      'name':"Pension tipo 1"
+    },
+    {
+      'id':"13",
+      'name':"Pension tipo 2"
+    }
+  ];
 
-  constructor() { }
+  public pensionerTypeOptions : Array<any> = [
+    {
+      'id':"12",
+      'name':"Pensionado  tipo 1"
+    },
+    {
+      'id':"13",
+      'name':"Pensionado tipo 2"
+    }
+  ];
+
+  public countryOptions : Array<any> = [
+    {
+      'id':"12",
+      'name':"Colombia"
+    },
+    {
+      'id':"13",
+      'name':"Argentina"
+    }
+  ];
+  constructor() {
+  }
 
   ngOnInit() {
   }
