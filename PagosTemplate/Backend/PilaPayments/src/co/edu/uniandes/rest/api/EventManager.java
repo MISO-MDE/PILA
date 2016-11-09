6f4f38c6f5f1023f5fbd2dd71aff13b119c46c34
japerea912 <george5015@gmail.com>
2016-11-09 00:31:42 -0500

    cambios en ws de novedad

--- PagosTemplate/Backend/PilaPayments/src/co/edu/uniandes/rest/api/EventManager.java
+++ PagosTemplate/Backend/PilaPayments/src/co/edu/uniandes/rest/api/EventManager.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -37,8 +38,9 @@ public class EventManager {
 	 * @return
 	 */
 	@GET
+	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getEvent(@QueryParam("id") String cedula) {
+	public String getEvent(@PathParam("id") String cedula) {
 
 		ObjectMapper mapper = new ObjectMapper();
 		String response = "";
