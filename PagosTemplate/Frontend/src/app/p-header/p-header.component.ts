c1ad509036a7ede427046d9d9e1e6eca2112bde7
rtaimal <rtaimal@gmail.com>
2016-11-09 22:59:55 -0500

    user email in header

--- PagosTemplate/Frontend/src/app/p-header/p-header.component.ts
+++ PagosTemplate/Frontend/src/app/p-header/p-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FireLoginService} from "../fire-login/fire-login.service";
 
 @Component({
   selector: 'app-p-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public fireLoginService :FireLoginService) { }
 
   ngOnInit() {
   }
