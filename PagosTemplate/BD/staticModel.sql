b8636a8914f00ae035ad21ce83d1b779205dd89e
rtaimal <rtaimal@gmail.com>
2016-11-24 22:15:53 -0500

    Static model

---
+++ PagosTemplate/BD/staticModel.sql
@@ -0,0 +1,14 @@
+    --ActividadEconomica
+    insert into ActividadEconomica (codigo, descripcion) values('8022','Establecimientos de educaci�n media');
+	insert into ActividadEconomica (codigo, descripcion) values('8513','Empresas dedicadas a practicas de odontologia');
+	insert into ActividadEconomica (codigo, descripcion) values('0117','Empresas dedicadas a la producci�n especializada de frutas, nueces, plantas bebestibles, y especias. Incluye el tostado y beneficio del cacao');
+	insert into ActividadEconomica (codigo, descripcion) values('1541','Empresa dedicada a la elaboracion de productos de molineria, de almidones,productos derivados del almidon y alimentos preparados para animales. Incluye el tostado de semillas y granos');
+	insert into ActividadEconomica (codigo, descripcion) values('1592','Empresa dedicada a la elaboracion de bebidas fermentadas no destiladas');
+	insert into ActividadEconomica (codigo, descripcion) values('1743','Empresa dedicada a la fabricaci�n de cuerdas, cordeles, cables, bramantes y redes');
+	insert into ActividadEconomica (codigo, descripcion) values('2101', 'Empresa dedicada a la fabricaci�n de papel, carton, y productos de papel y carton');
+	insert into ActividadEconomica (codigo, descripcion) values('2322', 'Empresa dedicada a la fabricaci�n de productos derivados del petroleo, fuera de refineria como asfalto');
+	insert into ActividadEconomica (codigo, descripcion) values('1431', 'Extraccion de esmeraldas');
+	insert into ActividadEconomica (codigo, descripcion) values('2321','Fabricacion de productos de la refinaci�n del petroleo, elaborados en refineria');
+
+
+
