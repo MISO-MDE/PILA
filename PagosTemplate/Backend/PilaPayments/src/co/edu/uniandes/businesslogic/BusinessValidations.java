09fac01f239e073b802c80b087ad936e763ed709
rtaimal <rtaimal@gmail.com>
2016-11-24 23:06:52 -0500

    se incluye clase validacion

--- PagosTemplate/Backend/PilaPayments/src/co/edu/uniandes/businesslogic/BusinessValidations.java
+++ PagosTemplate/Backend/PilaPayments/src/co/edu/uniandes/businesslogic/BusinessValidations.java
@@ -1,6 +1,9 @@
 package co.edu.uniandes.businesslogic;
 
+import java.util.ArrayList;
+
 import co.edu.uniandes.entity.PilaEntity;
+import co.edu.uniandes.entity.Validacion;
 
 public class BusinessValidations {
 	
@@ -16,6 +19,7 @@ public class BusinessValidations {
 	public String validations() {
 
 		String errorCondition = "";
+		ArrayList validaciones = new ArrayList<Validacion>();
 
 		String tipoPension = entity.getTipoPension().toString();
 		String tipoPensionado = entity.getTipoPensionado().toString();
@@ -32,19 +36,32 @@ public class BusinessValidations {
 		
 		String retorno = "";
 		if(logic.validarPensionPensionado(tipoPension, tipoPensionado) == null) {
+			
+			Validacion validacion1 = new Validacion();
+			validacion1.setName("Tipo Pension vs Tipo Pensionado inconsistente");
+			validacion1.getValues().add(tipoPension);
+			validacion1.getValues().add(tipoPensionado);
+			
 			error1 =	"{" +    
 								"\"" + "Validacion\":" + "\"Tipo Pension vs Tipo Pensionado inconsistente\"," +
 								"\"" + "valor1\":" + "\""+ tipoPension +"\"," +
 								"\"" + "valor2\":" + "\""+ tipoPensionado +"\"" +
 							"}";
+			validaciones.add(validacion1);
 		}
 		
 		if(logic.validarPensionadoPagador(tipoPensionado, tipoPagador) == null) {
+			
+			Validacion validacion2 = new Validacion();
+			validacion2.setName("Tipo Pension vs Tipo Pensionado inconsistente");
+			validacion2.getValues().add(tipoPension);
+			validacion2.getValues().add(tipoPensionado);
 			error2 = "{" + 
 					"\"" + "Validacion\":" + "\"Tipo Pensionado vs Tipo Pagador inconsistente\"," +
 					"\"" + "valor1\":" + "\"Vejez\"," +
 					"\"" + "valor2\":" + "\"Empleador\"" +
 				"}";
+			validaciones.add(validacion2);
 		}
 		
 //		if(!error1.isEmpty() || !error2.isEmpty()) {
@@ -67,6 +84,9 @@ public class BusinessValidations {
 					"}" +
 			"]" +
 		 */
+		
+		System.out.println(validaciones.size());
+		
 		return errorCondition;
 	}
 }
