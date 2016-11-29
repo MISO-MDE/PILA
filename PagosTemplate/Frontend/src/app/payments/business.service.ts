13e9e91825b33f02beec6fc14b0a54394e2353db
rtaimal <rtaimal@gmail.com>
2016-11-28 20:25:26 -0500

    cargar creados recientes

--- PagosTemplate/Frontend/src/app/payments/business.service.ts
+++ PagosTemplate/Frontend/src/app/payments/business.service.ts
@@ -2,21 +2,46 @@ import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {PaymentApiService} from "./rest.api.service";
+import {PensionerApiService} from "../pensioner/rest.api.service";
 @Injectable()
 export class PaymentsBusinessService {
-  public rows: Array<any>; //Toma los datos en el On Init de esta clase
+  public rows: Array<any>;
 
   public columns: Array<any> = [
-    {title: 'Nombre', name: 'firstName'},
-    {title: 'Apellido', name: 'lastName'},
+    {title: 'Nombre', name: 'nombre'},
+    {title: 'Apellido', name: 'apellido'},
     {title: 'Monto', name: 'amount'},
     {title: 'Fecha', name: 'paymentDate'},
     {title: 'Estado', name: 'status'}
   ];
 
   public selectedRow = {};
+  public selectedEntity: any = {};
+  public showValidations = false;
 
-  constructor(private paymentApiService: PaymentApiService) {
+
+  constructor(private paymentApiService: PaymentApiService,
+              private pensionerService: PensionerApiService) {
+  }
+
+  calculatePayment() {
+    this.paymentApiService.getPaymentCalculation(this.selectedEntity.cedula)
+      .subscribe((paymentObj: any) => {
+        if (paymentObj.errorCondition != null) {
+          this.rows = paymentObj.errorCondition;
+          this.showValidations = true;
+        }
+        else {
+          this.selectedRow = paymentObj
+        }
+      });
+  }
+
+  loadEntity() {
+    this.pensionerService.loadById(this.selectedEntity.cedula)
+      .subscribe((entityObj: any) => {
+        this.selectedEntity = entityObj;
+      });
   }
 
   public loadPayments(): any {
