dd781660b49d2a06b601a27f3d492c637a923199
rtaimal <rtaimal@gmail.com>
2016-11-30 17:18:04 -0500

    refactor: se crea relaciones entre tablas

--- Unified/segundainstancia/PaymentStaticModel/gen/generated/validationsDBUtil.java
+++ Unified/segundainstancia/PaymentStaticModel/gen/generated/validationsDBUtil.java
@@ -34,41 +34,41 @@ public class DBUtil {
 
 	       //Inserts tabla  Tipo Pensionado;			    
 			
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(1,'1','Pensionado de régimen de prima media. Tope máximo de pensión 25 smlmv");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(2,'2','Pensionado de régimen de prima media. Sin tope máximo de pensión");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(3,'3','Pensionado de régimen de ahorro individual. No aplica tope máximo de pensión");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(4,'4','Pensionado de riesgos laborales. Tope máximo de 25 smlmv");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(5,'5','Pensionado por el empleador. Con tope maximo de pension de 25 smlmv");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(6,'6','Pensionado por el empleador sin tope máximo de pensión");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(7,'7','Pensionado de entidades de los regimenes especial y de excepción, con tope máximo de pensión de 25 smlmv");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(8,'8','Pensionado de entidades de los regimenes especial y de excepción, sin tope máximo de pensión");	
-			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(9,'9','Beneficiario UPC adicional");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(1,'1','Pensionado de régimen de prima media. Tope máximo de pensión 25 smlmv')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(2,'2','Pensionado de régimen de prima media. Sin tope máximo de pensión')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(3,'3','Pensionado de régimen de ahorro individual. No aplica tope máximo de pensión')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(4,'4','Pensionado de riesgos laborales. Tope máximo de 25 smlmv')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(5,'5','Pensionado por el empleador. Con tope maximo de pension de 25 smlmv')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(6,'6','Pensionado por el empleador sin tope máximo de pensión')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(7,'7','Pensionado de entidades de los regimenes especial y de excepción, con tope máximo de pensión de 25 smlmv')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(8,'8','Pensionado de entidades de los regimenes especial y de excepción, sin tope máximo de pensión')");	
+			st.executeUpdate("insert into TipoPensionado (id, codigo, descripcion) values(9,'9','Beneficiario UPC adicional')");	
 	       //Inserts tabla  Tipo Pagador;			    
 			
-			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(1,'1','Empleador");	
-			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(2,'2','Administración de pensiones");	
-			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(3,'3','Pagador de pensiones");	
-			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(4,'4','Pensiones de entidades de los regimenes especial y de excepción");	
+			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(1,'1','Empleador')");	
+			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(2,'2','Administración de pensiones')");	
+			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(3,'3','Pagador de pensiones')");	
+			st.executeUpdate("insert into TipoPagador (id, codigo, descripcion) values(4,'4','Pensiones de entidades de los regimenes especial y de excepción')");	
 	       //Inserts tabla  Tipo Pension;			    
 			
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(1,'1','Vejez");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(2,'2','Sobrevivencia vitalicia riesgo comun");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(3,'3','Sobrevivencia temporal riesgo comun");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(4,'4','Sobrevivencia temporal riesgo comun, Conyugue o compañera(o) menor de 30 años sin hijos");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(5,'5','Sobrevivencia vitalicia por riesgo laboral");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(6,'6','Sobrevivencia temporal por riesgo laboral");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(7,'7','Sobrevivencia temporal por riesgo laboral, Conyugue o compañera(o) menor de 30 años sin hijos");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(8,'8','Invalidez riesgo común");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(9,'9','Invalidez resgo laboral");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(10,'10','Jubilacion");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(11,'11','Jubilacion para compartir");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(12,'12','Sanción");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(13,'13','Convencional");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(14,'14','Convencional para compartir");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(15,'15','Gracia");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(16,'16','Por convenio internacional");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(17,'17','Sentencia Judicial");	
-			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(18,'18','Conciliaciones");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(1,'1','Vejez')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(2,'2','Sobrevivencia vitalicia riesgo comun')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(3,'3','Sobrevivencia temporal riesgo comun')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(4,'4','Sobrevivencia temporal riesgo comun, Conyugue o compañera(o) menor de 30 años sin hijos')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(5,'5','Sobrevivencia vitalicia por riesgo laboral')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(6,'6','Sobrevivencia temporal por riesgo laboral')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(7,'7','Sobrevivencia temporal por riesgo laboral, Conyugue o compañera(o) menor de 30 años sin hijos')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(8,'8','Invalidez riesgo común')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(9,'9','Invalidez resgo laboral')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(10,'10','Jubilacion')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(11,'11','Jubilacion para compartir')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(12,'12','Sanción')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(13,'13','Convencional')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(14,'14','Convencional para compartir')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(15,'15','Gracia')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(16,'16','Por convenio internacional')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(17,'17','Sentencia Judicial')");	
+			st.executeUpdate("insert into TipoPension (id, codigo, descripcion) values(18,'18','Conciliaciones')");	
 
             st.executeUpdate("commit");
             //Relations           
